410a42575dbe5a8f686e73bf676eafba4829a74e
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-02-16 18:41:37 -0500

    Player Movement + Jump / No Double Jump
    Player Movement + Jump / No Double Jump

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // keycodes
+    KeyCode jump = KeyCode.Space;
 
     //components
     public Rigidbody rigid;
 
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float jumpForce = 5.0f;
+
+    // boolean
+    private bool isJumpig = false;
+
+
+    // layer
+    
 
 
     private void Start()
@@ -19,8 +28,35 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        Run();
+        Jump();
+    }
+
+
+    public void Run()
+    {
         rigid.velocity = new Vector3(moveSpeed, rigid.velocity.y, rigid.velocity.z);
     }
 
+    public void Jump()
+    {
+        if (Input.GetKeyDown(jump))
+        {
+            if (!isJumpig)
+            {
+                //rigid.velocity = new Vector3(rigid.velocity.x, jumpForce, rigid.velocity.z);
+                rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isJumpig = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumpig = false;
+        }
+    }
 }
 
