181afcfa1a1568599025868e7be67882e904bc34
kvv(Kevin Junho Kim) <59503331+kevinkim910408@users.noreply.github.com>
2021-03-19 23:45:56 -0400

    Sound - Coin

--- Assets/_Scripts/CoinManager.cs
+++ Assets/_Scripts/CoinManager.cs
@@ -8,18 +8,32 @@ public class CoinManager : MonoBehaviour
     public int coinScore;
 
     // sounds
-
+    AudioSource audioSource;
+    [SerializeField] AudioClip coinClip = null;
     // Start is called before the first frame update
     void Start()
     {
         scoreManager = FindObjectOfType<ScoreManager>();
-
+        audioSource = GameObject.FindGameObjectWithTag("CoinSFX").GetComponent<AudioSource>();
     }
+
+    public void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "COIN":
+                audioSource.clip = coinClip;
+                break;
+        }
+        audioSource.Play();
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            PlaySound("COIN");
             scoreManager.coinCount += 1;
             scoreManager.scoreCount += 100;
 
